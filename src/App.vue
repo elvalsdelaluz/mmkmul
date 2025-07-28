71b25ca4a8b821eba4cd600d757dacb2edf53a4d
Mariangeles Muñoz Loza <marialanus97@gmail.com>
2025-07-27 22:03:04 -0300

    Modifiqué el css base

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,26 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <p>mmkmul</p>
-  </header>
+  <nav class="app-header">
+      <h1>mmkmul</h1>
+      <button class="config-button" @click="openConfig">
+        <img src="./assets/images/configurar.png" alt="Configuración">
+      </button>
+  </nav>
 </template>
+
+<style>
+.config-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+}
+
+.config-button img {
+  width: 24px;
+  height: 24px;
+  display: block; /* Elimina espacio inferior de imágenes inline */
+}
+</style>
