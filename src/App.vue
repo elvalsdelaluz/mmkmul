bdf669b5d7e68326933229fd18061a87a4e47822
Franco Kumichel <francokumichel1996@gmail.com>
2025-07-27 22:23:29 -0300

    feat(Navbar): Agrego estilo base del navbar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav class="app-header">
-      <h1>mmkmul</h1>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand"><img src="./assets/images/logo.png" alt="Logo de compañia mmkmul"></a>
       <button class="config-button" @click="openConfig">
         <img src="./assets/images/configurar.png" alt="Configuración">
       </button>
+    </div>
   </nav>
 </template>
 
