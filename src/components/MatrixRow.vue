4bf1c9addf68984ffa3b3ac07c06b135a2498da5
Franco Kumichel <francokumichel1996@gmail.com>
2025-08-12 19:09:23 -0300

    feat(Matrix): Se define un componente base de matrices y su implementación con CSS Grid

--- src/components/MatrixRow.vue
+++ src/components/MatrixRow.vue
@@ -2,17 +2,64 @@
 </script>
 
 <template>
- 
+  <div class="grid" style="--bs-columns: 3; --bs-gap: 2rem;">
+    <div class="grid border p-2" style="--bs-columns: 4; --bs-gap: 0.5rem;">
+      <div class="g-col-1 border text-center">1</div>
+      <div class="g-col-1 border text-center">2</div>
+      <div class="g-col-1 border text-center">3</div>
+      <div class="g-col-1 border text-center">4</div>
+      <div class="g-col-1 border text-center">5</div>
+      <div class="g-col-1 border text-center">6</div>
+      <div class="g-col-1 border text-center">7</div>
+      <div class="g-col-1 border text-center">8</div>
+      <div class="g-col-1 border text-center">9</div>
+      <div class="g-col-1 border text-center">10</div>
+      <div class="g-col-1 border text-center">11</div>
+      <div class="g-col-1 border text-center">12</div>
+      <div class="g-col-1 border text-center">13</div>
+      <div class="g-col-1 border text-center">14</div>
+      <div class="g-col-1 border text-center">15</div>
+      <div class="g-col-1 border text-center">16</div>
+    </div>
 
-  <div class="row text-center g-3">
-    <div class="col-6 bg-primary text-white p-3">Col 6</div>
-    <div class="col-6 bg-secondary text-white p-3">Col 6</div>
-    <div class="col-6 bg-success text-white p-3">Col 6</div>
-    <div class="col-6 bg-danger text-white p-3">Col 6</div>
+    <div class="grid border p-2" style="--bs-columns: 4; --bs-gap: 0.5rem;">
+      <div class="g-col-1 border text-center">1</div>
+      <div class="g-col-1 border text-center">2</div>
+      <div class="g-col-1 border text-center">3</div>
+      <div class="g-col-1 border text-center">4</div>
+      <div class="g-col-1 border text-center">5</div>
+      <div class="g-col-1 border text-center">6</div>
+      <div class="g-col-1 border text-center">7</div>
+      <div class="g-col-1 border text-center">8</div>
+      <div class="g-col-1 border text-center">9</div>
+      <div class="g-col-1 border text-center">10</div>
+      <div class="g-col-1 border text-center">11</div>
+      <div class="g-col-1 border text-center">12</div>
+      <div class="g-col-1 border text-center">13</div>
+      <div class="g-col-1 border text-center">14</div>
+      <div class="g-col-1 border text-center">15</div>
+      <div class="g-col-1 border text-center">16</div>
+    </div>
+
+    <div class="grid border p-2" style="--bs-columns: 4; --bs-gap: 0.5rem;">
+      <div class="g-col-1 border text-center">1</div>
+      <div class="g-col-1 border text-center">2</div>
+      <div class="g-col-1 border text-center">3</div>
+      <div class="g-col-1 border text-center">4</div>
+      <div class="g-col-1 border text-center">5</div>
+      <div class="g-col-1 border text-center">6</div>
+      <div class="g-col-1 border text-center">7</div>
+      <div class="g-col-1 border text-center">8</div>
+      <div class="g-col-1 border text-center">9</div>
+      <div class="g-col-1 border text-center">10</div>
+      <div class="g-col-1 border text-center">11</div>
+      <div class="g-col-1 border text-center">12</div>
+      <div class="g-col-1 border text-center">13</div>
+      <div class="g-col-1 border text-center">14</div>
+      <div class="g-col-1 border text-center">15</div>
+      <div class="g-col-1 border text-center">16</div>
+    </div>
   </div>
-
-
-
 </template>
 
 
